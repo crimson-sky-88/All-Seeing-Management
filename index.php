7537414a020296bd90b746d1da87bb55d0070332
Kyle Daniel Javines <javineskyle123@gmail.com>
2024-03-19 23:12:01 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All-Seeing Management</title>
+</head>
+<body>
+    <header>
+        <nav>
+            
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
