dfcb428a5d823c05793155105bbb4254c6c6be34
Kyle Daniel Javines <javineskyle123@gmail.com>
2024-03-20 20:38:55 +0800

    implemented basic html framework for the landing page

--- index.php
+++ index.php
@@ -7,9 +7,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>All-Seeing Management</title>
+    <title>LOG IN</title>
 </head>
 <body>
-    
+    <header>
+        <div class="site-page-title">
+            <h1>All-Seeing Management</h1>
+        </div>
+    </header>
+    <main>
+        <div class="landing-page-album">
+
+        </div>
+
+        <div class="log-in-form">
+            <form>
+                <label for="username">Username: </label>
+                <input type="text" placeholder="SecretSauce" name="username">
+                <br>
+                <label for="password">Password: </label>
+                <input type="password" name="password">
+            </form>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
